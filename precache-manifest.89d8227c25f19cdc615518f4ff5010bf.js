24f2b394c21ce5d5b263d1e363724f10f32562ec
afrontend <autoscripts@gmail.com>
2020-03-11 21:59:24 +0900

    Updates

---
+++ precache-manifest.89d8227c25f19cdc615518f4ff5010bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0f2d747f4b0308b4291",
+    "url": "/fp-tetris-game/static/js/runtime~main.c0f2d747.js"
+  },
+  {
+    "revision": "115d008ff00465501154",
+    "url": "/fp-tetris-game/static/js/main.115d008f.chunk.js"
+  },
+  {
+    "revision": "e77fbb4d9156f1c66c6f",
+    "url": "/fp-tetris-game/static/js/1.e77fbb4d.chunk.js"
+  },
+  {
+    "revision": "115d008ff00465501154",
+    "url": "/fp-tetris-game/static/css/main.c49e6a1e.chunk.css"
+  },
+  {
+    "revision": "30ce5ab06e444dc3e3e59eb01a7de728",
+    "url": "/fp-tetris-game/index.html"
+  }
+];
