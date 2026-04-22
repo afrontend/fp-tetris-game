24279a6a6f179faeb7fb66fb379b5b5b0c531189
afrontend <autoscripts@gmail.com>
2026-04-22 19:32:48 +0900

    refactor: convert App class component to functional component
    Replace class-based App with hooks (useState, useEffect, useRef) for improved readability and modern React patterns.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import * as keyboard from 'keyboard-handler';
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import _ from 'lodash';
 import './App.css';
 import fpTetris from 'fp-tetris';
@@ -43,65 +43,61 @@ const getKeySymbol = keyValue => {
   return found ? found.keySymbol : null;
 }
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.savedState = null;
-    this.state = fpTetris.init({ rows: 17, columns: 12 });
-    this.state.timer = setInterval(() => {
-      this.setState(state => fpTetris.tick(state));
+function App() {
+  const [state, setState] = useState(() => fpTetris.init({ rows: 17, columns: 12 }));
+  const savedState = useRef(null);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setState(s => fpTetris.tick(s));
     }, 700);
 
     keyboard.keyPressed(e => {
       setTimeout(() => {
-        this.setState(state => {
+        setState(s => {
           const symbol = getKeySymbol(e.which);
           if (symbol === 'save') {
-            this.savedState = _.cloneDeep(state);
-            return state;
+            savedState.current = _.cloneDeep(s);
+            return s;
           } else if (symbol === 'reload') {
-            return this.savedState ? this.savedState : state;
+            return savedState.current ? savedState.current : s;
           } else {
-            return symbol ? fpTetris.key(symbol, state) : state;
+            return symbol ? fpTetris.key(symbol, s) : s;
           }
         });
       });
     });
-  }
 
-  componentWillUnmount() {
-    clearInterval(this.state.timer);
-  }
+    return () => clearInterval(timer);
+  }, []);
 
-  render() {
-    return args.debug
-      ? (
-        <div style={{columns: '400px 3'}}>
-          <div className="container">
-            <div className="App">
-              <Blocks window={_.flatten(fpTetris.toArray(this.state)[0])} />
-            </div>
-          </div>
-          <div className="container">
-            <div className="App">
-              <Blocks window={_.flatten(fpTetris.toArray(this.state)[1])} />
-            </div>
-          </div>
-          <div className="container">
-            <div className="App">
-              <Blocks window={_.flatten(fpTetris.join(this.state))} />
-            </div>
-          </div>
-        </div>
-      )
-      : (
+  return args.debug
+    ? (
+      <div style={{columns: '400px 3'}}>
         <div className="container">
           <div className="App">
-            <Blocks window={_.flatten(fpTetris.join(this.state))} />
+            <Blocks window={_.flatten(fpTetris.toArray(state)[0])} />
           </div>
         </div>
-      );
-  }
+        <div className="container">
+          <div className="App">
+            <Blocks window={_.flatten(fpTetris.toArray(state)[1])} />
+          </div>
+        </div>
+        <div className="container">
+          <div className="App">
+            <Blocks window={_.flatten(fpTetris.join(state))} />
+          </div>
+        </div>
+      </div>
+    )
+    : (
+      <div className="container">
+        <div className="App">
+          <Blocks window={_.flatten(fpTetris.join(state))} />
+        </div>
+      </div>
+    );
 }
 
 export default App;
